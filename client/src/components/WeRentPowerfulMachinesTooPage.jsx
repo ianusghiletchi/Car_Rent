5421ad8cac27474768bdd981109dc7aca39927e1
Ianuş <103447553+ianusghiletchi@users.noreply.github.com>
2024-01-23 16:07:59 +0200

    Fixed some scss bugs

--- client/src/components/WeRentPowerfulMachinesTooPage.jsx
+++ client/src/components/WeRentPowerfulMachinesTooPage.jsx
@@ -7,7 +7,7 @@ function WeRentPowerfulMachinesTooPage() {
     <div className="WRPMT-Main-Div">
       <div className="WRPMT-Text">
         <h1 style={{ textAlign: 'center', width: '80%', margin: '0 auto' }}>We Rent Powerful Machines Too</h1>
-        <p style={{ width: '80%', textAlign: 'center', margin: '0 auto', paddingTop: '0.5%' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Demikros, nisl sit amet ultricies lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl sit amet</p>
+        <p style={{ width: '80%', textAlign: 'center', margin: '0 auto', paddingTop: '0.5%' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Demikros, nisl sit amet ultricies lacinia, nisl nisl aliquam nisl.</p>
       </div>
       <div className="WRPMT-Image-Div shadow">
       {/* This is where the image will be using scss background-img */}
